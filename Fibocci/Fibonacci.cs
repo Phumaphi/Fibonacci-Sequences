854a12a5882cbb11dd0d9f205f7be0f9249c5245
nzonzonone@gmail.com <nzonzonone@gmail.com>
2020-01-08 22:03:31 +0200

    init

---
+++ Fibocci/Fibonacci.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Fibocci
+{
+    public class Fibonacci
+    {
+        public List<int> EvenSequence = new List<int>();
+
+        public List<int> FiboSequence = new List<int>();
+
+        public void FibonacEngine(int limit =12)
+        {
+
+            int number = 0, number1 = 1, number2 = 0;
+            int sum = 0;
+
+            while (sum <= limit)
+            {
+                number2 = number + number1;
+
+                if (number2 % 2 == 0)
+                {
+                    sum += number2;
+
+                    EvenSequence.Add(number2);
+                }
+                FiboSequence.Add((number2));
+                number = number1;
+                number1 = number2;
+            }
+        }
+    }
+}
---
+++ Fibocci/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fibocci
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            #region Fibonacci class initialization
+
+            var fibonac = new Fibonacci();
+
+            fibonac.FibonacEngine(5000000);
+
+            Console.WriteLine();
+            #endregion
+
+            #region Even-valued terms in a Fibonacci
+
+            foreach (var evenValue in fibonac.EvenSequence)
+            {
+                Console.WriteLine($" even-valued term: {evenValue}");
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Count of even-valued terms: {fibonac.EvenSequence.Count}");
+            Console.WriteLine();
+            Console.WriteLine($"Sum of even-valued terms in a Fibonacci " +
+                              $"sequence\nwhere the sum of those terms" +
+                              $" does not exceed 5 million is {fibonac.EvenSequence.Sum()} ");
+
+            #endregion
+
+            Console.WriteLine();
+
+            #region Fibonacci Sequence numbers
+
+            foreach (var i in fibonac.FiboSequence)
+            {
+               // Console.WriteLine($"  {i}");
+
+            }
+
+            Console.WriteLine($"Count of all fibonacci sequence" +
+                              $" numbers within 5 million: {fibonac.FiboSequence.Count}");
+
+            #endregion
+
+            Console.ReadKey();
+        }
+
+    }
+}
+
